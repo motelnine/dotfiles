67974fd75f03e249a6de6a0af87ada9efaf690c9
Matthew Smailey <them4tthew@gmail.com>
2025-04-10 16:37:31 -0700

    oops

--- vim/nvim-installplugins.sh
+++ vim/nvim-installplugins.sh
@@ -4,9 +4,23 @@ mkdir -p ~/.config/nvim/autoload
 mkdir -p ~/.config/nvim/bundle
 mkdir -p ~/.config/nvim/colors
 
-# Install vim-plug for NeoVim
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+# Install vim-plug (modern alternative to pathogen for NeoVim)
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Download plugins
+git clone https://github.com/maximbaz/lightline-ale ~/.config/nvim/bundle/lightline-ale
+git clone https://github.com/itchyny/lightline.vim ~/.config/nvim/bundle/lightline
+git clone https://github.com/scrooloose/nerdtree ~/.config/nvim/bundle/nerdtree
+git clone https://github.com/VundleVim/Vundle.vim ~/.config/nvim/bundle/Vundle.vim
+git clone https://github.com/editorconfig/editorconfig-vim.git ~/.config/nvim/bundle/editorconfig-vim
+git clone https://github.com/mxw/vim-jsx.git ~/.config/nvim/bundle/vim-jsx
+git clone https://github.com/ryanoasis/vim-devicons ~/.config/nvim/bundle/vim-devicons
+git clone https://github.com/airblade/vim-gitgutter ~/.config/nvim/bundle/vim-gitgutter
+git clone https://github.com/fatih/vim-go.git ~/.config/nvim/bundle/vim-go
+git clone https://github.com/dense-analysis/ale.git ~/.config/nvim/bundle/ale
+git clone https://github.com/isRuslan/vim-es6.git ~/.config/nvim/bundle/vim-es6
+#git clone https://github.com/wfxr/minimap.vim ~/.config/nvim/bundle/minimap
 
 # Download colorscheme
 wget https://raw.githubusercontent.com/Reewr/vim-monokai-phoenix/master/colors/monokai-phoenix.vim -O ~/.config/nvim/colors/monokai-phoenix.vim 
