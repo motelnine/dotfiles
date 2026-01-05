b9345de0099f26ba12cc46e947dedb7da6fb48ab
Matthew Smailey <them4tthew@gmail.com>
2026-01-05 00:58:50 -0700

    syncdown.sh auto commit

---
+++ radeon/cc-layout.sh
@@ -0,0 +1,7 @@
+xrandr \
+                    --output DisplayPort-0 --primary --mode 2560x1440 --pos 3840x0 --rotate normal \
+                    --output DisplayPort-1 --mode 3840x2160 --pos 0x0 --rotate normal \
+                    --output HDMI-A-0 --mode 3840x2160 --pos 6400x0 --rotate normal \
+                    --output HDMI-A-1 --mode 1920x720 --pos 3840x1440 --rotate normal
+
+xrandr --output DisplayPort-0 --mode 2560x1440 --rate 120
