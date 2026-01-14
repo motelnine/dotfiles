b06fdf85f27b1e0b2fe64461481694ce307303a1
Matthew Smailey <them4tthew@gmail.com>
2026-01-13 17:12:13 -0700

    radeon layout fixes

--- radeon/cc-layout.sh
+++ radeon/cc-layout.sh
@@ -1,7 +1,5 @@
 xrandr \
-                    --output DisplayPort-0 --primary --mode 2560x1440 --pos 3840x0 --rotate normal \
-                    --output DisplayPort-1 --mode 3840x2160 --pos 0x0 --rotate normal \
-                    --output HDMI-A-0 --mode 3840x2160 --pos 6400x0 --rotate normal \
-                    --output HDMI-A-1 --mode 1920x720 --pos 3840x1440 --rotate normal
-
-xrandr --output DisplayPort-0 --mode 2560x1440 --rate 120
+    --output DP-1 --primary --mode 2560x1440 --rate 165 --pos 3840x0 --rotate normal \
+    --output DP-2 --mode 3840x2160 --pos 0x0 --rotate normal \
+    --output HDMI-1 --mode 3840x2160 --pos 6400x0 --rotate normal \
+    --output HDMI-2 --mode 1920x720 --pos 3840x1440 --rotate normal
